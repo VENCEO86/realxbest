e14c059befc735652fd0e2256d95526b88df282c
VENCEO86 <venturebrothers12@gmail.com>
2025-11-11 12:39:03 +0900

    fix dbconfig.php connection info

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,6 +1,7 @@
 <?php
-$mysql_host = '';
-$mysql_user = '';
-$mysql_password = '';
-$mysql_db = '';
+$mysql_host = 'sql12.freesqldatabase.com';
+$mysql_user = 'sql12807141';     // freesqldatabase.com에서 받은 아이디
+$mysql_password = 'UljWTwX56L';   // freesqldatabase.com에서 받은 비밀번호
+$mysql_db = 'sql12807141';       // freesqldatabase.com에서 받은 DB 이름
+$mysql_port = '3306';
 ?>
