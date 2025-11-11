c8c4809272c0214c56033aa06c8f0911430ac80d
VENCEO86 <venturebrothers12@gmail.com>
2025-11-11 12:47:20 +0900

    fix db host port connection

--- data/dbconfig.php
+++ data/dbconfig.php
@@ -1,7 +1,6 @@
 <?php
-$mysql_host = 'sql12.freesqldatabase.com';
-$mysql_user = 'sql12807141';     // freesqldatabase.com에서 받은 아이디
-$mysql_password = 'UljWTwX56L';   // freesqldatabase.com에서 받은 비밀번호
-$mysql_db = 'sql12807141';       // freesqldatabase.com에서 받은 DB 이름
-$mysql_port = '3306';
+$mysql_host = 'sql12.freesqldatabase.com:3306';
+$mysql_user = 'sql12807141';
+$mysql_password = 'UljWTwX56L';
+$mysql_db = 'sql12807141';
 ?>
