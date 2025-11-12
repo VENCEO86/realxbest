02dfe4288ba3d42c3636fc35c8f6701004cd49bc
VENCEO86 <venturebrothers12@gmail.com>
2025-11-12 11:18:24 +0900

    Delete theme/dbconfig.php

--- theme/dbconfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-$g5['db']['host'] = G5_MYSQL_HOST;
-$g5['db']['user'] = G5_MYSQL_USER;
-$g5['db']['pass'] = G5_MYSQL_PASSWORD;
-$g5['db']['name'] = G5_MYSQL_DB;
-$g5['db']['port'] = G5_MYSQL_PORT;
